639439f1bec47799fa6ddee840c60c754062f884
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-02 19:26:07 +0200

    Project setup with tailwind css

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <h1>Loader section</h1>
+
+    );
+
+}
+
+export default Loader;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ const Navbar = () => {
     return (
         <h1>Navbar section</h1>
 
-    )
+    );
 
 }
+
+export default Navbar;
