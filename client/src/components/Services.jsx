4d221d06459d18599180bcc4dc0bb9f3efb95db9
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-02 19:38:34 +0200

    Project setup with tailwind css

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,7 @@
+const Services = () => {
+    return (
+        <h1>Sevices section</h1>
+
+    );
+}
+export default Services;
---
+++ client/src/components/Transaction.jsx
@@ -0,0 +1,6 @@
+const Transaction = () => {
+    return (
+        <h1>Transaction section</h1>
+    );
+}
+export default Transaction;
