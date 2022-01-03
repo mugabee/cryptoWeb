d48240327997a47a82d06ffbaec17bb23ad6f2ec
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-03 21:49:47 +0200

    Designig the navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-      <h1 className="w-32 cursor-pointer"> Crypto Web </h1>
+      <h1 className="w-32 cursor-pointer text-white text-3xl font-bold"> CRYPTOWEB </h1>
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
