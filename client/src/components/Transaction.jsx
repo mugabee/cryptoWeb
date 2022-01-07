8ece8e4a555b73911b6a39481ec5d62f279a9bed
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-07 22:11:42 +0200

    transaction

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,6 +1,7 @@
-const Transaction = () => {
-    return (
-        <h1>Transaction section</h1>
-    );
-}
-export default Transaction;
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+import useFetch from "../hooks/useFetch";
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
