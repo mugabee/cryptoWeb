3004ce2c5dd30623ab68984f7fa931e0596f8733
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-14 21:56:19 +0200

    Components Building

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,17 @@
-import { Footer, Welcome, Navbar, Transaction, Services } from './components'
 
-const App = () => {
-  return (
-    <div className="min-h-screen">
-
-      <div className="gradient-bg-welcome">
-      
-        <Navbar />
-       <Welcome /> 
-
-        </div>
-        
-      
-       <Footer />  
-      <Transaction />  
-      <Services /> 
-         
-        
-
-    
-    </div>
    
-  );
-}
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
