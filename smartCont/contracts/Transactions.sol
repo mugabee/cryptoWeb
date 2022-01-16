f6de55caefa806f00f1e03fd0c340ff9ee455a59
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-16 21:21:03 +0200

    Components Building

--- smartCont/contracts/Transactions.sol
+++ smartCont/contracts/Transactions.sol
@@ -4,19 +4,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Transactions {
-    uint256 transactionCount;
 
-    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-  
-    struct TransferStruct {
-        address sender;
-        address receiver;
-        uint amount;
-        string message;
-        uint256 timestamp;
-        string keyword;
-    }
 
     TransferStruct[] transactions;
 
