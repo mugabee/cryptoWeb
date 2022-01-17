41308ce1542c06cc3c806ceb626dd7e7d64a816d
mugabe richard <mugabe.richard.37@gmail.com>
2022-01-17 20:10:50 +0200

    Components Building

--- smartCont/scripts/deploy.js
+++ smartCont/scripts/deploy.js
@@ -6,3 +6,14 @@ const main = async () => {
   
     console.log("Transactions address: ", transactionsContract.address);
   };
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
